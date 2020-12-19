7a5bbfb162b27a521abc378f78065c9841921216
Dani Bednarski <bedz@me.com>
2020-12-20 01:26:54 +1100

    Changed to minimise use of jQuery to meet rubric.

--- js/index.js
+++ js/index.js
@@ -1,253 +1,52 @@
+// Initialise the taskManager object
+const taskManager = new TaskManager(0);
+
 // Select the New Task Form
-// const newTaskForm = document.querySelector('#newTaskForm');
-const newTaskForm = document.querySelector('#taskContainer');
+const newTaskForm = document.querySelector('#btn-add-task');
 
-// Add an 'onsubmit' event listener
-newTaskForm.addEventListener('submit', (event) => {
-// Prevent default action of browswer refreshing
+// New task event listener
+newTaskForm.addEventListener('click', event => {
   event.preventDefault();
+
+  // New task values template
+  const newTaskNameInput = document.querySelector('#newTaskNameInput');
+  const name = newTaskNameInput.value;
+  const taskId = taskManager.tasks.length;
+  const type = '';
+  const description = '';
+  const assignedTo = '';
+  const priority = '';
+  const dueDate = '';
+
+  taskManager.addTask(taskId, type, name, description, assignedTo, priority, dueDate);
+
+  // Render the tasks
+  taskManager.render();
+  
+  // Clear newTaskNameInput value
+  newTaskNameInput.value = '';
 });
-  
-function isValidTaskInput(data) {
-  return data !== null && data !== '';
-}
 
-function isValidDateInput(data) {
-  return /\w\w\w\s\d\d.\d\d.\d\d/i.test(data);
-}
+// Delete button event listener
+document.body.addEventListener('click', function (event) {
+  if (event.target.classList == 'bin') {
+    taskManager.deleteTask(this.taskId);
+    taskManager.render();
+  };
+});
 
-//Render Function of 1 Task Card
-function renderTask(taskId,taskName) {
-  $('#mainContainer')
-      .append($([
-          '<div class= "card row mx-auto text-center" id='+taskId +'>',
-          '<div class="col">',
-          ' <div class="row bg-dark mx-n3 pt-3">',
-          '  <div class="col-1 bg-dark">',
-          '<div class="form-group">',
-          ' <label for="type'+taskId+'">Type</label>',
-          ' <select id="type'+taskId+'" class="selectpicker mr-n3" data-width="fit">',
-          '     <option class="bg-dark" value="none"',
-          '       data-content="<span class=\'btn btn-outline-primary btn-shrink btn-none\'>None</span>">None',
-          '   </option>',
-          '  <option class="bg-dark" value="work"',
-          '      data-content="<span class=\'btn btn-outline-primary btn-shrink btn-none\'>Work</span>">Work',
-          '  </option>',
-          '  <option class="bg-dark" value="leisure"',
-          '      data-content="<span class=\'btn btn-outline-primary btn-shrink btn-none\'>Leisure</span>">',
-          '      Leisure</option>',
-          '  <option class="bg-dark" value="other"',
-          '       data-content="<span class=\'btn btn-outline-primary btn-shrink btn-none\'>Other</span>">Other',
-          '  </option>',
-          ' </select>',
-          ' </div>',
-          ' </div>',
-
-           '   <div class="col-5 bg-dark p-desc description">',
-         '                       <div class="input-group">',
-           '                           <label for="desc'+taskId+'">Task/Description</label>',
-           '                           <input id="desc'+taskId+'" type="text" class="form-control bg-dark text-white input rounded task-desc cardTag" placeholder="Type Task Name here..." value="'+taskName+'">',
-           '<button id="btn-description" type="button"',
-           ' class="more text-white px-3 py-2 rounded">...</button>',
-           '     </div>',
-           '<div class="descriptionDiv" style="display: none">',
-          '<input id="description1" type="text" class="form-control bg-dark text-white input rounded task-desc cardTag" placeholder="Description..">',,
-           '</div>',
-           '                       </div>',
-           '     <div class="col-1 bg-dark text-center">',
-           '                           <div class="form-group">',
-           '                               <label for="assigned'+taskId+'">Assigned</label>',
-           '                               <select id="assigned'+taskId+'" class="bg-dark selectpicker text-center mr-n3" data-width="fit">',
-           '                                   <option class="bg-dark" value="none"',
-           '                                       data-content="<span class=\'btn btn-outline-primary btn-shrink btn-none\'>&nbsp;None&nbsp;</span>">None',
-           '         </option>',
-           '                                   <option class="bg-dark" value="victoria"',
-           '                                       data-content="<span class=\'btn btn-victoria btn-shrink\'>&nbsp;Victoria&nbsp;</span>">Victoria',
-           '         </option>',
-           '                                   <option class="bg-dark" value="dani"',
-           '                                       data-content="<span class=\'btn btn-dani btn-shrink\'>&nbsp;&nbsp;&nbsp;Dani&nbsp;&nbsp;&nbsp;</span>">Dani',
-           '         </option>',
-           '                               </select>',
-           '                           </div>',
-           '                       </div>',
-
-
-           ' <div class="col-1 bg-dark">',
-           '                       <div class="form-group">',
-           '                            <label for="priority'+taskId+'">Priority</label>',
-           '                            <select id="priority'+taskId+'" class="bg-dark selectpicker mr-n3" data-width="fit">',
-           '                                <option class="bg-dark" value="none"',
-           '                                    data-content="<span class=\'btn btn-outline-primary btn-shrink btn-none\'>&nbsp;None&nbsp;</span>">None',
-           '      </option>',
-           '                                <option class="bg-dark" value="low"',
-           '                                   data-content="<span class=\'btn btn-success btn-shrink\'>&nbsp;&nbsp;&nbsp;Low&nbsp;&nbsp;&nbsp;</span>">Low',
-           '     </option>',
-           '                               <option class="bg-dark" value="medium"',
-           '                                    data-content="<span class=\'btn btn-warning btn-shrink\'>&nbsp;Medium&nbsp;</span>">Medium',
-           '     </option>',
-           '                               <option class="bg-dark" value="high"',
-           '                                   data-content="<span class=\'btn btn-danger btn-shrink\'>&nbsp;&nbsp;&nbsp;High&nbsp;&nbsp;&nbsp;</span>">',
-           '                                   High</option>',
-           '                           </select>',
-           '                       </div>',
-           '                   </div>',
-
-
-           ' <div class="col-1 bg-dark">',
-           '                         <div class="form-group">',
-           '                             <label for="status'+taskId+'">Status</label>',
-           '                             <select id="status'+taskId+'" class="bg-dark selectpicker mr-n3" data-width="fit">',
-           '                                 <option class="bg-dark" value="none"',
-           '                                     data-content="<span class=\'btn btn-outline-primary btn-shrink btn-none\'>&nbsp;None&nbsp;</span>">None',
-           '       </option>',
-           '                                 <option class="bg-dark" value="not-started"',
-           '                                     data-content="<span class=\'btn btn-danger btn-status btn-shrink btn-wider\'>Not Started</span>">',
-           '                                     Not',
-           '         Started</option>',
-           '                                 <option class="bg-dark" value="in-progress"',
-           '                                     data-content="<span class=\'btn btn-warning btn-status btn-shrink btn-wider\'>In Progress</span>">',
-           '                                     In',
-           '         Progress</option>',
-           '                                 <option class="bg-dark" value="completed"',
-           '                                     data-content="<span class=\'btn btn-success btn-status btn-shrink\'> Complete </span>">',
-           '                                     Completed</option>',
-           '                             </select>',
-           '                        </div>',
-           '                     </div>',
-
-           '<div class="col-2 bg-dark text-white">',
-           '                       <div class="form-group">',
-           '                            <label for="date'+taskId+'">Due Date</label>',
-           '                            <input id="date'+taskId+'" placeholder="&#xf271;"',
-           '                                class="date bg-dark text-white text-center rounded border-0 date py-2" type="text">',
-           '  </div>',
-           '                        </div>',
-
-           ' <div class="col-1 bg-dark">',
-           '                             <div class="form-group">',
-           '                                 <label for="delete1">Delete</label>',
-           '                                 <p id="delete1" class="bin" >&#xf2ed;</p>',
-           '                             </div>',
-           '                        </div>',
-           '                   </div > ',
-           '               </div > ',
-           '          </div > ',
-           '          </div > '
-
-      ].join("\n"))
-      );
-
-}
-
-function addEventHandlers(taskId,taskManager1) {
-  //Hook bin button handling
-  $('#'+taskId+' .bin').on('click', function () {  
-    taskManager1.deleteTask(taskId);
-  $("#"+taskId).remove();
-  })
-
-  //Hook type selection handling
-  $('#type'+taskId).on('change', function () {  
-    alert(taskId);
-    taskManager1.updateTaskType(taskId,$(this).val()); 
-    let t=taskManager1.getTask(taskId);
-    console.log("New task type: "+t.taskType);
-  })
-
-  //Hook assignee selection handling
-  $('#assigned'+taskId).on('change', function () {  
-    console.log('Assignee');
-    taskManager1.updateAssignee(taskId,$(this).val()); 
-    let t=taskManager1.getTask(taskId);
-    console.log("New task assignee: "+t.assignee);
-  })
-
-  //Hook priority selection handling
-  $('#priority'+taskId).on('change', function () {  
-    console.log('Priority');
-    taskManager1.updatePriority(taskId,$(this).val()); 
-    let t=taskManager1.getTask(taskId);
-    console.log("New task priority: "+t.priority);
-  })
-
-  //Hook status selection handling
-  $('#status'+taskId).on('change', function () {  
-    console.log('Status');
-    taskManager1.updateStatus(taskId,$(this).val()); 
-    let t=taskManager1.getTask(taskId);
-    console.log("New task status: "+t.status);
-  })
-
-  //Hook date selection handling
-  $('#date'+taskId).on('change', function () {  
-    console.log('Date');
-    taskManager1.updateDueDate(taskId,$(this).val()); 
-    let t=taskManager1.getTask(taskId);
-    console.log("New task date: "+t.dueDate);
-  })
-
-}
-
-//Document Ready
-$(document).ready(function () {
-  let taskManager1 = new taskManager();
-  let $newTask = $('#newTaskNameInput');
-  let taskId=taskManager1.createTask('none', $newTask.val(), 'none', 'none', 'none', 'none', null);
-  renderTask(taskId,$newTask.val());
-//Hook description button handling
-$('.description button').on('click', function () {
-  $(this).parent().parent().find('div.descriptionDiv').toggle();
-  }
- )
-
- addEventHandlers(taskId,taskManager1);
-
-  //Validation for create task
-  $('#btn-add-task').on("click", function () {
-    let emptyTask = false;
-    if ($newTask.val().length > 1) {
-      let tasks = $('input.cardTag');
-      for (let i = 0; i < tasks.length; i++) {
-        if ($(tasks[i]).val() == '') {
-          emptyTask = true;
-          break;
-        }
-      }
-      //change this condition to only false for validation of using provided cards 
-      if (emptyTask == false || emptyTask == true) {
-        $('#alertMessage').hide();
-        //creating a default task         
-        let taskId=taskManager1.createTask('none', $newTask.val(), 'none', 'none', 'none', 'none', null);
-        renderTask(taskId,$newTask.val());
-
-        $('#'+taskId+' .description button').on('click', function () {
-          $(this).parent().parent().find('div.descriptionDiv').toggle();
-          })
-
-         //Deleting task on Bin click
-         $('#'+taskId+' .bin').on('click', function () {                
-          taskManager1.deleteTask(taskId);
-          $("#"+taskId).remove();
-          })
-
-         //Event Handler
-         addEventHandlers(taskId,taskManager1);
-
-          $newTask.val("");
-          $('select').selectpicker();
-          getCalendar();
-        }
-
-      else {
-        $('#alertMessage').text('Please use the empty slots provided')
-          .show();
-      }
+// Show/hide intro message
+document.body.addEventListener('click', function (event) {
+  if (event.target.id == 'btn-add-task' || event.target.classList == 'bin') {
+    if (taskManager.tasks.length > 0) {
+      // Remove class from container (displays intro message)
+      const taskList = document.getElementById("taskList");
+      taskList.classList.remove("taskList");
+    } else {
+      taskList.classList.add("taskList");
+      taskList.innerHTML = `<div id="intro">
+          You currently have no tasks added. Click the green '+' button to add a task.
+        </div>`;
     }
-    else {
-      $('#alertMessage').show();
-    }
-
-  }); 
-
-  
+  };
 });
--- js/taskManager.js
+++ js/taskManager.js
@@ -1,115 +1,195 @@
-// taskManager Class
-class taskManager {
+class TaskManager {
     constructor() {
-        this.tasks = [];        
-    }
-    
-    // Adds task
-    createTask(taskType, taskName, taskDescription, assignee, priority, status, dueDate) {
-        const newTask = {
-            taskId: this.tasks.length+1,
-            taskType,        
-            taskName,
-            taskDescription,
-            assignee,
-            priority,
-            status,
-            dueDate,
-        }
-        this.tasks.push(newTask);
-        //this.renderTask(newTask.taskId,newTask.taskName);
-        return newTask.taskId;
+        this.tasks = [];
     }
 
-    getTasks() {
-        return this.tasks;
+    addTask(taskId, type = 'none', name, description, assignedTo = 'none', priority = 'none', dueDate = '') {
+        const task = {
+            taskId: taskId,
+            type: type,
+            name: name,
+            description: description,
+            assignedTo: assignedTo,
+            priority: priority,
+            status: status,
+            dueDate: dueDate,
+        };
+
+        this.tasks.push(task);
     }
-    
-    // Updates Task Type
+  
+    // Return an array of all tasks
+    getTasks() {
+          return this.tasks;
+    }
+  
+    // Get task by taskId
+    getTask(taskId) {
+      for(let i=0;i<this.tasks.length;i++) {
+        if(this.tasks[i].taskId==taskId) {
+          return this.tasks[i];
+        }
+      }
+    }
+      
+    // Update task type
     updateTaskType(taskId, taskType) {
         let index= this.findTask(taskId);
         this.tasks[index].taskType = taskType;
     }
-  
-    // Updates Task Name
+    
+    // Update task name
     updateTaskName(taskId, taskName) {
         let index= this.findTask(taskId);
         this.tasks[index].taskName = taskName;
     }
 
-    // Update Task Description
+    // Update task description
     updateTaskDescription(taskId, taskDescription) {
         let index= this.findTask(taskId);
         this.tasks[index].taskDescription = taskDescription;
     }
     
-    // Updates Assignee
+    // Update assignee
     updateAssignee(taskId, assignee) {
         let index= this.findTask(taskId);
         this.tasks[index].assignee = assignee;
     }
   
-    // Updates Priority
+    // Update priority
     updatePriority(taskId, priority) {
         let index= this.findTask(taskId);
         this.tasks[index].priority = priority;
     }
     
-    // Updates Status
+    // Update status
     updateStatus(taskId, status) {
         let index= this.findTask(taskId);
         this.tasks[index].status = status;
     }
     
-    // Updates Due Date
+    // Update due date
     updateDueDate(taskId, dueDate) {
         let index= this.findTask(taskId);
         this.tasks[index].dueDate = dueDate;
     }
     
-    // Deletes Task
+    // Delete task
     deleteTask(taskId) {
-        let index= this.findTask(taskId);
-        this.tasks.splice(index,1);          
+        this.tasks.splice(taskId, 1);        
     }
 
-    //find the index of the task in task array by taskId
-    findTask(taskId) {
-        for(let i=0;i<this.tasks.length;i++) {
-            if(this.tasks[i].taskId==taskId) {
-                return i;
-            }
-        }
-    } 
-    getTask(taskId)
-    {
-        for(let i=0;i<this.tasks.length;i++) {
-            if(this.tasks[i].taskId==taskId) {
-                return this.tasks[i];
-            }
-        }
+    // Render the task list, initialise date pickers and selectors
+    render() {
+      // Create an array to store the tasks' HTML
+      const tasksHtmlList = [];
+
+      // Loop over our tasks and create the html, storing it in the array
+      for (let i = 0; i < this.tasks.length; i++) {
+          // Get the current task in the loop
+          const task = this.tasks[i];
+
+          // Create the task html
+          const taskHtml = createTaskHtml(task.taskId, task.type, task.name, task.description, task.assignedTo, task.priority, task.status, task.dueDate);
+
+          // Push it to the tasksHtmlList array
+          tasksHtmlList.push(taskHtml);
+      }
+
+      // Create the tasksHtml by joining each item in the tasksHtmlList
+      // with a new line in between each item.
+      const tasksHtml = tasksHtmlList.join('\n');
+
+      // Set the inner html of the tasksList on the page
+      const tasksList = document.querySelector('#taskList');
+      tasksList.innerHTML = tasksHtml;
+    
+    // Make dueDate field clickable
+    getCalendar();
+
+    // Make selectors active
+    $('select').selectpicker();
     }
-  
 }
 
+// Create the HTML for a task
+const createTaskHtml = (taskId, type, name, description, assignedTo, priority, status, dueDate) => `
+<div class="card row mx-auto text-center" id="${taskId}">
+  <div class="col">
+    <div class="row bg-grey mx-n3 pt-3">
+      <div class="col-1 bg-grey">
+        <div class="form-group">
+          <label for="type${taskId}">Type</label>
+          <select id="type${taskId}" class="selectpicker mr-n3" data-width="fit">
+            <option class="bg-grey" value="none" data-content="<span class='btn btn-outline-primary btn-shrink btn-none'>None</span>">None</option>
+            <option class="bg-grey" value="work" data-content="<span class='btn btn-outline-primary btn-shrink btn-none'>Work</span>">Work</option>
+            <option class="bg-grey" value="leisure" data-content="<span class='btn btn-outline-primary btn-shrink btn-none'>Leisure</span>">Leisure</option>
+            <option class="bg-grey" value="other" data-content="<span class='btn btn-outline-primary btn-shrink btn-none'>Other</span>">Other</option>
+          </select>
+        </div>
+      </div>
 
-//Testing for console
-/*
-let taskManager1 = new taskManager();
-taskManager1.createTask('work','task 3','victoria', 'low', 'not started', '12/12/2020');
-    
-    let tasks = taskManager1.getTasks();
-    for(let i = 0; i < tasks.length; i++){
-        console.log(tasks[i].taskId);
-        console.log(tasks[i].taskName);
-        console.log(tasks[i].taskType);
-        console.log(tasks[i].dueDate);
-    }
+      <div class="col-5 bg-grey p-desc description">
+        <div class="input-group">
+          <label for="name${taskId}">Task/Description</label>
+          <input id="name${taskId}" type="text" class="form-control bg-grey text-white input rounded task-desc" placeholder="Type task name here.." value="${name}">
+          <button id="descriptionBtn${taskId}" type="button" class="more text-white px-3 py-2 rounded">...</button>
+        </div>
+        <div id="descriptionDiv${taskId} class="descriptionDiv" style="display: none">
+          <input id="description${taskId}" type="text" class="form-control bg-grey text-white input rounded task-desc" placeholder="Description..">
+        </div>
+      </div>
 
-    console.log('get task id=1');
+      <div class="col-1 bg-grey text-center">
+        <div class="form-group">
+          <label for="assigned${taskId}">Assigned</label>
+          <select id="assigned${taskId}" class="bg-grey selectpicker text-center mr-n3" data-width="fit">
+            <option class="bg-grey" value="none" data-content="<span class='btn btn-outline-primary btn-shrink btn-none'>&nbsp;None&nbsp;</span>">None</option>
+            <option class="bg-grey" value="victoria" data-content="<span class='btn btn-victoria btn-shrink'>&nbsp;Victoria&nbsp;</span>">Victoria</option>
+            <option class="bg-grey" value="dani" data-content="<span class='btn btn-dani btn-shrink'>&nbsp;&nbsp;&nbsp;Dani&nbsp;&nbsp;&nbsp;</span>">Dani</option>
+          </select>
+        </div>
+      </div>
 
-    let t=taskManager1.getTask(1);
-    console.log(t.taskType);
-*/
-    
-   
+      <div class="col-1 bg-grey">
+        <div class="form-group">
+          <label for="priority${taskId}">Priority</label>
+          <select id="priority${taskId}" class="bg-grey selectpicker mr-n3" data-width="fit">
+            <option class="bg-grey" value="none" data-content="<span class='btn btn-outline-primary btn-shrink btn-none'>&nbsp;None&nbsp;</span>">None</option>
+            <option class="bg-grey" value="low" data-content="<span class='btn btn-success btn-shrink'>&nbsp;&nbsp;&nbsp;Low&nbsp;&nbsp;&nbsp;</span>">Low</option>
+            <option class="bg-grey" value="medium" data-content="<span class='btn btn-warning btn-shrink'>&nbsp;Medium&nbsp;</span>">Medium</option>
+            <option class="bg-grey" value="high" data-content="<span class='btn btn-danger btn-shrink'>&nbsp;&nbsp;&nbsp;High&nbsp;&nbsp;&nbsp;</span>">High</option>
+          </select>
+        </div>
+      </div>
+
+      <div class="col-1 bg-grey">
+        <div class="form-group">
+          <label for="status${taskId}">Status</label>
+          <select id="status${taskId}" class="bg-grey selectpicker mr-n3" data-width="fit">
+            <option class="bg-grey" value="none" data-content="<span class='btn btn-outline-primary btn-shrink btn-none'>&nbsp;None&nbsp;</span>">None</option>
+            <option class="bg-grey" value="not-started" data-content="<span class='btn btn-danger btn-status btn-shrink btn-wider'>Not Started</span>">Not Started</option>
+            <option class="bg-grey" value="in-progress" data-content="<span class='btn btn-warning btn-status btn-shrink btn-wider'>In Progress</span>">In Progress</option>
+            <option class="bg-grey" value="completed" data-content="<span class='btn btn-success btn-status btn-shrink'> Complete </span>">Completed</option>
+          </select>
+        </div>
+      </div>
+
+      <div class="col-2 bg-grey text-white">
+        <div class="form-group">
+          <label for="date${taskId}">Due Date</label>
+          <input id="date${taskId}" placeholder="&#xf271;"
+            class="date bg-grey text-white text-center rounded border-0 date py-2" type="text">
+        </div>
+      </div>
+
+      <div class="col-1 bg-grey">
+        <div class="form-group">
+          <label for="delete1">Delete</label>
+          <p id="delete1" class="bin">&#xf2ed;</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+`
