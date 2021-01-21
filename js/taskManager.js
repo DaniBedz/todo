a49193a1fd47060658ef4282c1a2bae5019a322b
Dani Bednarski <bedz@me.com>
2021-01-21 11:34:32 +1100

    Reverse priority sort order (more logical)

--- js/taskManager.js
+++ js/taskManager.js
@@ -252,7 +252,7 @@ export class TaskManager {
 
   // Sort taskPriority ascending
   sortByTaskPriorityAsc() {
-    const priority = ['none', 'low', 'medium', 'high'];
+    const priority = ['high', 'medium', 'low', 'none'];
   
     taskManager.tasks.sort((a, b) => {
       let aIndex = priority.findIndex(priority => a.taskPriority.includes(priority));
